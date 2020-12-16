e6155f8d77ae46d121721fd481ef95704f631381
Abinash Raju <54245267+BlackHatThamizhan@users.noreply.github.com>
2020-12-16 09:03:13 +0500

    Add files via upload

---
+++ Mac_Changer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import subprocess
+
+def banner():
+    B_Text = '''
+    ███╗   ███╗██████╗    ██████╗ ██╗      █████╗  ██████╗██╗  ██╗██╗  ██╗ █████╗ ████████╗
+    ████╗ ████║██╔══██╗   ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██║  ██║██╔══██╗╚══██╔══╝
+    ██╔████╔██║██████╔╝   ██████╔╝██║     ███████║██║     █████╔╝ ███████║███████║   ██║
+    ██║╚██╔╝██║██╔══██╗   ██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██╔══██║██╔══██║   ██║
+    ██║ ╚═╝ ██║██║  ██║██╗██████╔╝███████╗██║  ██║╚██████╗██║  ██╗██║  ██║██║  ██║   ██║
+    ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝
+                        Mac_Changer V2.0
+                        Coded by Abinash Raju
+
+    '''
+    print(B_Text)
+print(banner())
+
+Interface = input("Enter Your Interface: \n")
+NewMac = input("Enter New Mac: \n")
+print("Changing Your Mac Address " + Interface + "To" + NewMac)
+
+subprocess.call(["sudo ifconfig", Interface, "down"])
+subprocess.call(["sudo ifconfig", Interface, "hw", "ether", NewMac])
+subprocess.call(["sudo ifconfig", Interface, "up"])
